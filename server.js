ffa5df2c269e38653c1afdb8a0d56bd958b5e24b
Valérie <valerie@Valerieventures.fritz.box>
2018-12-13 09:44:36 +0100

    Add Portfolio view and link in navigation

--- server.js
+++ server.js
@@ -37,7 +37,13 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page'
+    pageTitle: 'About'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio'
   });
 });
 
